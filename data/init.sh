f0276232b2c16900540f6b9eb806774d0dd2b440
Benjamin Ramser <legionaerr@googlemail.com>
2022-12-04 14:18:23 +0100

    chore: add cleanup

--- data/init.sh
+++ data/init.sh
@@ -3,4 +3,5 @@
 sleep 3
 psql -h postgis -p 5432 -U postgres -d bonn < ./data/bonn_example.sql
 psql -h postgis -p 5432 -U postgres -d bonn -f ./data/sql/add_index.sql
-
+rm ./data/bonn_example.sql
+rm ./data/._bonn_example.sql
